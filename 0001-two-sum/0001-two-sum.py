0f1d07ecd808d68c110dad050cbbe1a6d5d5d543
CHRISTIAN PARFAIT <80205858+Me710@users.noreply.github.com>
2024-02-19 13:12:12 +0100

    Time: 36 ms (84.89%), Space: 12.3 MB (96.89%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int 
+        :rtype: List[int]
+        """
+        num_map = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in num_map:
+                return [num_map[diff], i] 
+            num_map[n] = i
