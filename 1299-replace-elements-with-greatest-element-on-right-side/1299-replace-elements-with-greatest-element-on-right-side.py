38f0c6d345c78d6c1fe9f9913842a9d5a5922fd2
CHRISTIAN PARFAIT <80205858+Me710@users.noreply.github.com>
2024-02-27 14:46:41 +0100

    Time: 544 ms (63.78%), Space: 13.4 MB (59.27%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,9 @@
+class Solution:
+    def replaceElements(self, arr):
+        n = len(arr)
+        max_right = -1  
+        for i in range(n-1, -1, -1):  
+            temp = arr[i]  
+            arr[i] = max_right  
+            max_right = max(max_right, temp)
+        return arr
